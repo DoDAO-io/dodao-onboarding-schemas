bc6995968c5c0a7ed44344b7383801335e81f6e3
robin <robinnagpal.tiet@gmail.com>
2022-01-30 11:23:49 -0500

    update space model

--- src/models/SpaceModel.ts
+++ src/models/SpaceModel.ts
@@ -1,8 +1,11 @@
 export interface SpaceModel {
   id: string;
   avatar?: string;
+  creator: string;
+  mission: string;
   name: string;
   network: string;
-  admins?: string[];
+  admins: string[];
+  members: string[];
   terms?: string;
 }
