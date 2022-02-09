50305b85ae145b2ead971eb374344533f0a34f45
robin <robinnagpal.tiet@gmail.com>
2022-02-09 08:44:11 -0500

    guide submission model

--- build/src/models/GuideModel.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QuestionType = void 0;
-var QuestionType;
-(function (QuestionType) {
-    QuestionType["SingleChoice"] = "SingleChoice";
-    QuestionType["MultipleChoice"] = "MultipleChoice";
-})(QuestionType = exports.QuestionType || (exports.QuestionType = {}));
