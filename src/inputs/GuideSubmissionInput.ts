6744cdf89f89f6db1e0a6f17db3db79e434561c0
robin <robinnagpal.tiet@gmail.com>
2022-02-09 15:04:10 -0500

    code nit

--- src/inputs/GuideSubmissionInput.ts
+++ src/inputs/GuideSubmissionInput.ts
@@ -2,9 +2,9 @@ import { GuideStepSubmission } from 'models/GuideSubmissionModel';
 
 export interface GuideSubmissionInput {
   from: string;
-  timestamp: string;
-  uuid: string;
   guideUuid: string;
   space: string;
   steps: GuideStepSubmission[];
+  timestamp: string;
+  uuid: string;
 }
